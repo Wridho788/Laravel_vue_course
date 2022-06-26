6bf61ab8b8310e86c7d6048ed9a2befc12dd637d
ridhoDMI <105182858+ridhoDMI@users.noreply.github.com>
2022-06-26 11:28:49 +0700

    Show loading spinner on Registration and login form submit button on clik

--- vue/src/views/Register.vue
+++ vue/src/views/Register.vue
@@ -62,9 +62,7 @@
         />
       </div>
       <div>
-        <label for="password_confirmation" class="sr-only"
-          >Password Confirmation</label
-        >
+        <label for="password_confirmation" class="sr-only">Password</label>
         <input
           id="password_confirmation"
           name="password_confirmation"
@@ -81,7 +79,12 @@
     <div>
       <button
         type="submit"
+        :disabled="loading"
         class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+        :class="{
+          'cursor-not-allowed': loading,
+          'hover:bg-indigo-500': loading,
+        }"
       >
         <span class="absolute left-0 inset-y-0 flex items-center pl-3">
           <LockClosedIcon
@@ -89,6 +92,27 @@
             aria-hidden="true"
           />
         </span>
+        <svg
+          v-if="loading"
+          class="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+        >
+          <circle
+            class="opacity-25"
+            cx="12"
+            cy="12"
+            r="10"
+            stroke="currentColor"
+            stroke-width="4"
+          ></circle>
+          <path
+            class="opacity-75"
+            fill="currentColor"
+            d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+          ></path>
+        </svg>
         Sign up
       </button>
     </div>
@@ -96,23 +120,30 @@
 </template>
 
 <script setup>
+import { ref } from "vue";
 import { LockClosedIcon } from "@heroicons/vue/solid";
-import { useRouter } from "vue-router";
-
 import store from "../store";
-
+import { useRouter } from "vue-router";
 const router = useRouter();
 const user = {
   name: "",
   email: "",
   password: "",
 };
+const loading = ref(false);
 function register(ev) {
   ev.preventDefault();
-  store.dispatch("register", user).then(() => {
-    router.push({
-      name: "Dashboard",
+  loading.value = true;
+  store
+    .dispatch("register", user)
+    .then(() => {
+      loading.value = false;
+      router.push({
+        name: "Dashboard",
+      });
+    })
+    .catch(() => {
+      loading.value = false;
     });
-  });
 }
 </script>
